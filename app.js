294a104ce8ac5ffdcc4cc751788175bd08dc262a
Apoorva Teli <apoorvateli@gmail.com>
2018-08-06 19:59:52 +0530

    Style campground show page

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var dbPath = process.env.DATABASEURL || "mongodb://localhost/go_camping";
 mongoose.connect(dbPath);
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 seedDB();
 
